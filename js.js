c0e1949b1673845572ea0494582e324fd588b5fb
ronakbhutiya <ronakbhutiya123@gmail.com>
2024-07-15 16:52:51 +0530

    add javascript

--- js.js
+++ js.js
@@ -1,1 +1,12 @@
-console.log("k");
+const linkIcons = document.querySelectorAll("#icon--link");
+const navView = document.querySelector(".nav-view ");
+const closeBtn = document.querySelector(".fa-xmark ");
+linkIcons.forEach((icon) => {
+  icon.addEventListener("click", () => {
+    navView.classList.toggle("view");
+    closeBtn.style.display = "block";
+  });
+});
+closeBtn.addEventListener("click", () => {
+  navView.classList.remove("view");
+});
